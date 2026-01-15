5179d6cd17fdbdd8d8d3aee693a7908f79b582f6
Roth Sorayuth <sorayuthroth@gmail.com>
2026-01-15 21:24:34 +0700

    fully completed

---
+++ 3 - Navigation and separate views/views/edit/edit.js
@@ -0,0 +1,171 @@
+// DOMS ELEMENTS  ---------------------------------------------------------
+const dom_questions_view = document.querySelector("#questions-view");
+const dom_questions_dialog = document.querySelector("#questions-dialog");
+
+// DATA  ---------------------------------------------------------
+let questions = [
+  {
+    title: "What does HTML stand for?",
+    choiceA: "Hi Thierry More Laught",
+    choiceB: "How To move Left",
+    choiceC: "Ho Theary Missed the Laundry !",
+    choiceD: "Hypertext Markup Language",
+    correct: "D",
+  },
+  {
+    title: "What does CSS stand for?",
+    choiceA: "Cisco and Super Start",
+    choiceB: "Ci So Sa",
+    choiceC: "Cascading Style Sheets ",
+    choiceD: "I don't know !",
+    correct: "C",
+  },
+  {
+    title: "What does JS stand for?",
+    choiceA: "Junior stars",
+    choiceB: "Justing Star",
+    choiceC: "Javascript",
+    choiceD: "RonanScript",
+    correct: "C",
+  },
+];
+
+let questionToEdit = null;
+
+
+function hide(element) {
+  element.style.display = "none";
+}
+
+function show(element) {
+  element.style.display = "block";
+}
+
+
+function saveQuestions() {
+  localStorage.setItem("questions", JSON.stringify(questions));
+}
+
+function loadQuestions() {
+  let questionsStorage = JSON.parse(localStorage.getItem("questions"));
+  if (questionsStorage !== null) {
+    questions = questionsStorage;
+  }
+}
+
+
+
+function renderQuestions() {
+  
+  dom_questions_container = document.querySelector("#questions-container");
+  dom_questions_container.remove();
+  dom_questions_container = document.createElement("div");
+  dom_questions_container.id = "questions-container";
+  dom_questions_view.appendChild(dom_questions_container);
+
+  
+  for (let index = 0; index < questions.length; index++) {
+    let question = questions[index];
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.dataset.index = index;
+    dom_questions_container.appendChild(card);
+
+    let questionInfos = document.createElement("div");
+    questionInfos.className = "question-info";
+    card.appendChild(questionInfos);
+
+    let title = document.createElement("spam");
+    title.className = "title";
+    title.textContent = question.title;
+    questionInfos.appendChild(title);
+
+    
+    let actions = document.createElement("div");
+    actions.className = "actions";
+    card.appendChild(actions);
+
+    let editAction = document.createElement("img");
+    editAction.src = "../../img/edit.svg";
+    editAction.addEventListener("click", editQuestion);
+    actions.appendChild(editAction);
+
+    let trashAction = document.createElement("img");
+    trashAction.src = "../../img/trash.png";
+    trashAction.addEventListener("click", removeQuestion);
+    actions.appendChild(trashAction);
+  }
+}
+
+function editQuestion(event) {
+  
+  questionToEdit = event.target.parentElement.parentElement.dataset.index;
+
+  
+  let question = questions[questionToEdit];
+  document.getElementById("title").value = question.title;
+  document.getElementById("choiceA").value = question.choiceA;
+  document.getElementById("choiceB").value = question.choiceB;
+  document.getElementById("choiceC").value = question.choiceC;
+  document.getElementById("choiceD").value = question.choiceD;
+
+ 
+  dom_createEditButton.textContent = "EDIT";
+  show(dom_questions_dialog);
+}
+
+function removeQuestion(event) {
+  
+  let index = event.target.parentElement.parentElement.dataset.index;
+
+  
+  questions.splice(index, 1);
+
+  
+  saveQuestions();
+
+
+  renderQuestions();
+}
+
+function onAddQuestion() {
+  show(dom_questions_dialog);
+}
+
+function onCancel(e) {
+  hide(dom_questions_dialog);
+}
+
+function onCreate() {
+  hide(dom_questions_dialog);
+
+  if (questionToEdit !== null) {
+    let editQuestion = questions[questionToEdit];
+    editQuestion.title = document.getElementById("title").value;
+    editQuestion.correct = "A";
+    editQuestion.choiceA = document.getElementById("choiceA").value;
+    editQuestion.choiceB = document.getElementById("choiceB").value;
+    editQuestion.choiceC = document.getElementById("choiceC").value;
+    editQuestion.choiceD = document.getElementById("choiceD").value;
+  } else {
+    let newQuestion = {};
+    newQuestion.title = document.getElementById("title").value;
+    newQuestion.correct = "A";
+    newQuestion.choiceA = document.getElementById("choiceA").value;
+    newQuestion.choiceB = document.getElementById("choiceB").value;
+    newQuestion.choiceC = document.getElementById("choiceC").value;
+    newQuestion.choiceD = document.getElementById("choiceD").value;
+    questions.push(newQuestion);
+  }
+
+  
+  saveQuestions();
+
+  renderQuestions();
+}
+
+
+loadQuestions();
+
+renderQuestions();
